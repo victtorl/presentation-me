de2c1faba2819aed330d796171be3b8a70e3740f
victtorl <lince00@outlook.com>
2025-02-03 11:11:57 -0500

    update proyects added

--- pages/proyectos/index.vue
+++ pages/proyectos/index.vue
@@ -45,6 +45,8 @@
   <script setup>
 
  import img1 from '~/assets/images/projects/electratec.png'
+ import img3 from '~/assets/images/projects/limafirma.png'
+ import img4 from '~/assets/images/projects/belanova.png'
 
   const proyectos = [
     {
@@ -74,6 +76,42 @@
       imageUrl:
         'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
       date: '14/01/2024',
+      datetime: '2020-03-16',
+      category: { title: 'sistema', href: '#' },
+      author: {
+        name: 'Victor Lara',
+        role: 'Co-Founder / CTO',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
+    {
+      id: 3,
+      title: 'Página de aterrizaje firma de abogados',
+      href: 'https://www.limafirma.pe/',
+      description:
+        'Página de aterrizaje de una web de firma de abogados',
+      imageUrl:img3,
+      date: '03/02/2025',
+      datetime: '2020-03-16',
+      category: { title: 'sistema', href: '#' },
+      author: {
+        name: 'Victor Lara',
+        role: 'Co-Founder / CTO',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
+    {
+      id: 4,
+      title: 'Buscador de canciones usando el api de Deezer',
+      href: 'https://search-songs-app.netlify.app/',
+      description:
+        'Buscador de canciones usando el api de Deezer',
+      imageUrl:img4,
+      date: '03/02/2025',
       datetime: '2020-03-16',
       category: { title: 'sistema', href: '#' },
       author: {
